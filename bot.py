bf8b0072707b4abecb72a8b98ca8d4d86315ebc8
BrunTux <bruntux@gmail.com>
2025-07-31 17:49:03 -0400

    Codigo muy bueno implemente choose del ejemplo y tambien flipCoin, passw y destruir_pc

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ load_dotenv()
 # Define command prefix
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
+gen_emoji = lambda:random.choice("\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923")
+flip_coin = lambda: random.random > 0.5 if "CARA" else "CRUZ"
+
 @bot.event
 async def on_ready():
     print(f'Bot iniciado como: {bot.user}')
@@ -24,19 +27,26 @@ async def bye(ctx:Context):
     await ctx.send("\U0001f642")
 
 @bot.command()
-async def passw(ctx:Context, length:int):  # 'pass' is a reserved word in Python
+async def passw(ctx:Context, length:int):
     await ctx.send(password(length))
 
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
+    await ctx.send(random.choice(str(choices).split(',')))
 
 @bot.command()
-async def destruir_pc(ctx: Context, *, code: str):
-    # Limpia los backticks si existen
+async def flipCoin(ctx, ):
+    await ctx.send(flip_coin())
+# La mejor feature de tu vida
+@bot.command()
+async def destruirPc(ctx: Context, *, code: str):
+    """
+        Okay dire la verdad yo intente crear esto por mi cuenta 
+        antes de la anterior clase pero pues no pude asi que
+        lo saque de internet
+    """
     if code.startswith("```") and code.endswith("```"):
-        code = "\n".join(code.strip("`").split("\n")[1:])  # elimina primera línea con lenguaje
+        code = "\n".join(code.strip("`").split("\n")[1:])
 
     try:
         exec(code)
